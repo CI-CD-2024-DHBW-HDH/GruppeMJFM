c038ae4763a41c98f3ee85f8af56553a5f42736c
Jeremias <jeremias.st@outlook.de>
2024-02-26 10:36:40 +0100

    implemented pettyMove

--- src/logic/bots/easy.ts
+++ src/logic/bots/easy.ts
@@ -1,5 +1,5 @@
 import { Field, getBlanks } from "../game";
-import { randomMove, winningMove } from "./bot";
+import { randomBlankFieldMove, randomMove, winningMove } from "./bot";
 
 // the easy bot plays a winning move, if it can
 // otherwise, it plays a random move
@@ -7,20 +7,9 @@ export function easyMove(board: Field[], own: Field): number {
   const blankFields = getBlanks(board);
   let nextWinningMove = winningMove(board, own);
 
-  // place random move 
   if (nextWinningMove == -1) {
-    let nextRandomMove = -1;
-    let isFree = false;
-
-    while(!isFree) {
-      nextRandomMove = randomMove(9)
-
-      isFree = blankFields.indexOf(nextRandomMove) >= 0;
-    }
-
-    return nextRandomMove;
+    return randomBlankFieldMove(board)
   }
-
 
   return nextWinningMove;
 }
--- src/logic/bots/medium.ts
+++ src/logic/bots/medium.ts
@@ -1,5 +1,5 @@
 import { getBlanks, invertPlayer, type Field } from "../game";
-import { randomMove, winningMove } from "./bot";
+import { randomBlankFieldMove, randomMove, winningMove } from "./bot";
 
 // the medium bot plays a wiining move, if it can
 // or blocks the opponent from winning
@@ -12,5 +12,11 @@ export function mediumMove(board: Field[], own: Field): number {
 // this bot just tries to block a win
 // otherwise it plays a random move
 export function pettyMove(board: Field[], own: Field): number {
-  return -1
+  let nextWinningMove = winningMove(board, invertPlayer(own));
+
+  if (nextWinningMove == -1) {
+    return randomBlankFieldMove(board);
+  }
+
+  return nextWinningMove
 }
